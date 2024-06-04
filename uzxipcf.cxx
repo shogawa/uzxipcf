9a63ea2c57f055bfb3ac6c62f4c104e830443cd2
shogawa <ogawakusas@gmail.com>
2024-06-04 20:02:23 +0900

    Update

--- uzxipcf.cxx
+++ uzxipcf.cxx
@@ -19,7 +19,7 @@
 #include <XSUtil/Numerics/Numerics.h>
 #include <XSFunctions/Utilities/FunctionUtility.h>
 extern "C" {
-void uzxipcf(const RealArray& energyArray, const RealArray& params,
+void Uzxipcf(const RealArray& energyArray, const RealArray& params,
     int spectrumNumber, RealArray& trans, RealArray& transErr,
     const string& initString)
 {
